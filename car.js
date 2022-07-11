6698f928c3bcdfb344749ed49332428b58924564
blam0101 <thienbao0712@gmail.com>
2022-07-11 11:49:45 -0700

    Sensor Rays Established

--- car.js
+++ car.js
@@ -11,11 +11,14 @@ class Car{
         this.friction = 0.05;
         this.angle = 0;
 
+
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update(){
+    update(roadBorders){
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move(){
@@ -75,5 +78,6 @@ class Car{
         ctx.fill();
 
         ctx.restore();
+        this.sensor.draw(ctx);
     }
 }
