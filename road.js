f48148d8e174375b9a4d2c0a91ce2c32fc18f9d8
blam0101 <thienbao0712@gmail.com>
2022-07-10 18:14:45 -0700

    Defining the Road
    Divide lanes, set line borders, set the car to the middle lane in the right position

--- road.js
+++ road.js
@@ -11,20 +11,49 @@ class Road{
         // y axis in computer grows downwards
         this.top = -infinity;
         this.bottom = infinity;
+
+        const topLeft = {x:this.left,y:this.top};
+        const topRight = {x:this.right,y:this.top};
+        const bottomLeft = {x:this.left,y:this.bottom};
+        const bottomRight = {x:this.right,y:this.bottom};
+        this.borders = [
+            [topLeft, bottomLeft],
+            [topRight, bottomRight]
+        ];
+    }
+
+    getLaneCenter(laneIndex){
+        const laneWidth = this.width/this.laneCount;
+        return this.left + laneWidth/2 + 
+            Math.min(laneIndex, this.laneCount - 1)*laneWidth; // |0|1|2|3|
+        
     }
 
     draw(ctx){
         ctx.lineWidth = 5;
         ctx.strokeStyle = "white";
 
-        ctx.beginPath();
-        ctx.moveTo(this.left, this.top);
-        ctx.lineTo(this.left, this.bottom);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(this.right, this.top);
-        ctx.lineTo(this.right, this.bottom);
-        ctx.stroke();
+        for (let i =0; i <= this.laneCount - 1; ++i){
+            const x = lerp(
+                this.left, 
+                this.right,
+                i/this.laneCount
+            );
+        
+            ctx.setLineDash([20, 20]); //Dash of 20px and a break of 20px
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
+        
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            ctx.beginPath();
+            ctx.moveTo(border[0].x, border[0].y);
+            ctx.lineTo(border[1].x, border[1].y);
+            ctx.stroke();
+        });
     }
 }
+
